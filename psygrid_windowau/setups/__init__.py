6dbe64aa7009a900c461cb8d518b8af958326f01
zahidshaikmohammed-cmyk <zahidshaikmohammed@gmail.com>
2026-09-23 01:46:59 +0530

    Implement Phase 3 setup-family detectors

---
+++ psygrid_windowau/setups/__init__.py
@@ -0,0 +1,5 @@
+"""Phase-3 independent XAUUSD setup-family detectors."""
+
+from .models import SetupCandidate, SetupFamily, SetupDirection
+
+__all__ = ["SetupCandidate", "SetupFamily", "SetupDirection"]
